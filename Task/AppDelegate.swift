ada52e9d1332d1da1dedd761ecf67a2e993299a1
Victor Mendes <victor_mendes1@icloud.com>
2021-12-21 22:37:29 -0300

    add extensão diretamente ao AppDelegate

--- Task/AppDelegate.swift
+++ Task/AppDelegate.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     let notificationCenter = UNUserNotificationCenter.current()
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         requestAuthForLocalNotifications()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
     func requestAuthForLocalNotifications() {
         notificationCenter.delegate = self
@@ -30,18 +30,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func scheduleLocalNotification(_ task: Task) {
-      //checking the notification setting whether it's authorized or not to send a request.
+        //checking the notification setting whether it's authorized or not to send a request.
         UNUserNotificationCenter.current().getNotificationSettings { settings in
             if settings.authorizationStatus == UNAuthorizationStatus.authorized{
-              //1. create contents
+                //1. create contents
                 let content = UNMutableNotificationContent()
                 content.title = task.title
                 content.body = task.notes ?? ""
-                    content.sound = UNNotificationSound.default
+                content.sound = UNNotificationSound.default
                 
                 //2. create trigger [calendar, timeinterval, location, pushnoti]
-               // let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0, repeats: false)
-                
                 let dataDue = Calendar.current.dateComponents([.year,.month,.day,.hour,.minute, .second], from: task.date)
                 let trigger = UNCalendarNotificationTrigger(dateMatching: dataDue, repeats: false)
                 //3. make a request
@@ -53,27 +51,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                         print("error in notification! ")
                     }
                 }
-           }
-           else {
-              print("user denied")
-           }
-       }
+            }
+            else {
+                print("user denied")
+            }
+        }
     }
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    
 }
